b2bb5568b4dc132a636107586cea66a3ac36a1ed
Cathleen Runde <cathleenrunde@new-host-2.home>
2021-10-14 08:54:27 -0700

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Route exact path='/'>
           <Home />
       </Route>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/screens/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import './Footer.css'
+
+const Footer = () =>
+  <footer>
+    <div className='github-linkedIn'>
+      <a
+            className="github"
+        href='https://github.com/runcatha'>
+        <img
+          id='github'
+          src='https://i.imgur.com/RshgsOc.png'
+          alt='Github'/>
+      </a>
+      <a  className="linkedIn"
+          href='https://www.linkedin.com/in/cathleen-runde-10692b204/'>
+        <img
+          id='linkedIn'
+          src='https://i.imgur.com/9yybFiy.png'
+          alt='LinkedIn'/>
+      </a>
+    </div>
+    </footer>
+
+export default Footer
