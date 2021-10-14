8403ea3123907594660ca9652f24960dccbad97a
Cathleen Runde <cathleenrunde@new-host-2.home>
2021-10-14 08:58:18 -0700

    about me

---
+++ client/src/screens/AboutMe/AboutMe.jsx
@@ -0,0 +1,9 @@
+import './AboutMe.css'
+
+export default function AboutMe() {
+  return (
+    <>
+      <h3>this is my about me</h3>
+    </>
+  )
+}
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,8 +1,10 @@
 import './Home.css'
+import AboutMe from '../AboutMe/AboutMe'
 
 export default function Home() {
   return (
     <>
+      <AboutMe />
       <h3>this is my home</h3>
     </>
   )
